6fa1831a375a8cf6323e69cf08f02c78acd5c129
ProfesorRafaelBiano87 <biano.rafael@escola.pr.gov.br>
2025-06-26 09:00:38 -0300

    .

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function inserirNome(){
 let nomeusuario = prompt ("Qual é o seu Nome");
 let elemento = document.querySelector("#Nome_usuario") ;
 elemento.textContent = nomeusuario;
-
 }
+inserirNome ();
