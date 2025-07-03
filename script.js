17f38fb2746ff47af6f1ad981b877866032f1e7d
ProfesorRafaelBiano87 <biano.rafael@escola.pr.gov.br>
2025-07-03 12:07:35 +0000

    03072025

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 
-function inserirNome(){
-let nomeusuario = prompt("Salve piazada Qual seu nome");
-let elemento = document.querySelector("#Nome_usuario") ;
-elemento.textContent = nomeusuario;
+function inserirNome() {
+    let nomeUsuario = prompt("Qual o seu nome?")
+    let elemento = document.querySelector("#nome_cursista");
+    elemento.textContent = nomeUsuario;
 }
-inserirNome ();
+inserirNome();
 
 let linguagens = ["JavaScript", "Python", "C++"];
 console.log(linguagens[0]) //"JavaScript";
 console.log(linguagens[1]) //Python;
 console.log(linguagens[2]) //C++;
-
-const intem = document.querySelector("#lista");
-intem.textContent = linguagens[2]
